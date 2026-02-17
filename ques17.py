b975725282c755a63935813b5a291b1c483a85c5
Himanshi Kumar <kumarhimanshi2004@gmail.com>
2026-02-17 08:44:18 +0530

    Create ques17.py

---
+++ ques17.py
@@ -0,0 +1,17 @@
+#Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+class Solution:
+    def merge(self, intervals):
+       
+        intervals.sort()
+        
+        result = []
+        
+        for interval in intervals:
+            
+            if not result or result[-1][1] < interval[0]:
+                result.append(interval)
+            
+            else:
+             result[-1][1] = max(result[-1][1], interval[1])
+        
+        return result
